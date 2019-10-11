9f607c8c5a7f01b6e46382c45094b15e3ebd7c0a
OlhaLu <olhalutsyshyna@gmail.com>
2019-10-11 17:41:56 +0300

    Updates

---
+++ precache-manifest.757388351aa721a9e77fb34b90dfc63e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb9faf14e0a82998ee2eeac3608bf8b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "48d47aa9615740173894",
+    "url": "/goit-react-hw-01-components/static/css/main.fc7894ea.chunk.css"
+  },
+  {
+    "revision": "c5c04ea3ce20d317fca3",
+    "url": "/goit-react-hw-01-components/static/js/2.3a632355.chunk.js"
+  },
+  {
+    "revision": "48d47aa9615740173894",
+    "url": "/goit-react-hw-01-components/static/js/main.b4ae377a.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
